faea4f31c11339fecb0a5019d2e71473f65c6531
nfmohit-wpmudev <nfmohit49@gmail.com>
2020-04-15 11:27:53 +0600

    Exemplary usage of the downloadBoilerplate function

--- app/commands/use/index.js
+++ app/commands/use/index.js
@@ -14,7 +14,7 @@ import logo from 'asciiart-logo';
  * Internal dependencies
  */
 import * as config from '../../../package.json';
-import { verifyBoilerplate } from './utils';
+import { verifyBoilerplate, downloadBoilerplate } from './utils';
 
 const Use = async ( boilerplate ) => {
 	console.log( logo( config ).render() );
@@ -25,15 +25,10 @@ const Use = async ( boilerplate ) => {
 	const isBoilerplateVerified = await verifyBoilerplate( boilerplate );
 	if ( isBoilerplateVerified ) {
 		console.log( `${ boilerplate } found!` );
+		downloadBoilerplate( boilerplate );
 	} else {
 		console.log( `We couldn't find ${ boilerplate }!` );
 	}
-
-	const isBoilerplateVerifiedSummary = await verifyBoilerplate(
-		boilerplate,
-		'summary'
-	);
-	console.log( isBoilerplateVerifiedSummary );
 };
 
 export default Use;
