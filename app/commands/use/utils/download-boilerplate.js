1cda91991c26042dc9511de0683605c66525d012
nfmohit-wpmudev <nfmohit49@gmail.com>
2020-04-16 07:23:24 +0600

    Introduced error handling to the downloadBoilerplate utlity function

--- app/commands/use/utils/download-boilerplate.js
+++ app/commands/use/utils/download-boilerplate.js
@@ -1,41 +1,63 @@
+/**
+ * Utility function to download a boilerplate
+ *
+ * @author Nahid Ferdous Mohit.
+ * @since  2.0.0
+ */
+
+/*
+ * External dependencies
+ */
 import fs from 'fs';
 import path from 'path';
 import axios from 'axios';
 import ProgressBar from 'progress';
 
-async function downloadBoilerplate( boilerplate ) {
-	const url = `http://catlin.local/boilerplates/${ boilerplate }/downloads/${ boilerplate }.zip`;
+/*
+ * Downloads a boilerplate
+ *
+ * @param {string} boilerplate - Slug of the boilerplate.
+ */
+const downloadBoilerplate = async ( boilerplate ) => {
+	try {
+		const url = `http://catlin.local/boilerplates/${ boilerplate }/downloads/${ boilerplate }.zip`;
 
-	console.log( 'Connecting…' );
-	const { data, headers } = await axios( {
-		url,
-		method: 'GET',
-		responseType: 'stream'
-	} );
-	const totalLength = headers[ 'content-length' ];
+		console.log( 'Connecting …' );
 
-	console.log( 'Starting download' );
-	const progressBar = new ProgressBar(
-		'-> downloading [:bar] :percent :etas',
-		{
-			width: 40,
-			complete: '=',
-			incomplete: ' ',
-			renderThrottle: 1,
-			total: parseInt( totalLength )
-		}
-	);
+		const { data, headers } = await axios( {
+			url,
+			method: 'GET',
+			responseType: 'stream'
+		} );
+		const totalLength = headers[ 'content-length' ];
 
-	const writer = fs.createWriteStream(
-		path.resolve(
-			__dirname,
-			'../../../../templates',
-			`${ boilerplate }.zip`
-		)
-	);
+		console.log( 'Starting download' );
 
-	data.on( 'data', ( chunk ) => progressBar.tick( chunk.length ) );
-	data.pipe( writer );
-}
+		const progressBar = new ProgressBar(
+			'-> downloading [:bar] :percent :etas',
+			{
+				width: 40,
+				complete: '=',
+				incomplete: ' ',
+				renderThrottle: 1,
+				total: parseInt( totalLength )
+			}
+		);
+
+		const writer = fs.createWriteStream(
+			path.resolve( __dirname, '../../../temp', `${ boilerplate }.zip` )
+		);
+
+		data.on( 'data', ( chunk ) => progressBar.tick( chunk.length ) );
+		data.pipe( writer );
+	} catch ( error ) {
+		console.log(
+			`An error occured!
+Error Status: ${ error.response.statusText }
+Error Code: ${ error.response.status }
+Error Message: ${ error }`
+		);
+	}
+};
 
 export { downloadBoilerplate };
