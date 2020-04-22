f83721b5125d5543dfb018010448836b1a3ee2bd
nfmohit-wpmudev <nfmohit49@gmail.com>
2020-04-22 09:18:32 +0600

    Re-organized the downloadBoilerplate function

--- app/commands/use/utils/download-boilerplate.js
+++ app/commands/use/utils/download-boilerplate.js
@@ -8,10 +8,7 @@
 /*
  * External dependencies
  */
-import fs from 'fs';
-import path from 'path';
 import axios from 'axios';
-import ProgressBar from 'progress';
 
 /*
  * Downloads a boilerplate
@@ -22,41 +19,17 @@ const downloadBoilerplate = async ( boilerplate ) => {
 	try {
 		const url = `http://catlin.local/boilerplates/${ boilerplate }/downloads/${ boilerplate }.zip`;
 
-		console.log( 'Connecting …' );
-
-		const { data, headers } = await axios( {
+		const { data } = await axios( {
 			url,
 			method: 'GET',
-			responseType: 'stream'
+			responseType: 'arraybuffer'
 		} );
-		const totalLength = headers[ 'content-length' ];
 
-		console.log( 'Starting download' );
+		const buffer = Buffer.from( data );
 
-		const progressBar = new ProgressBar(
-			'-> downloading [:bar] :percent :etas',
-			{
-				width: 40,
-				complete: '=',
-				incomplete: ' ',
-				renderThrottle: 1,
-				total: parseInt( totalLength )
-			}
-		);
-
-		const writer = fs.createWriteStream(
-			path.resolve( __dirname, '../../../temp', `${ boilerplate }.zip` )
-		);
-
-		data.on( 'data', ( chunk ) => progressBar.tick( chunk.length ) );
-		data.pipe( writer );
+		return buffer;
 	} catch ( error ) {
-		console.log(
-			`An error occured!
-Error Status: ${ error.response.statusText }
-Error Code: ${ error.response.status }
-Error Message: ${ error }`
-		);
+		return `Error Message: ${ error }`;
 	}
 };
 
