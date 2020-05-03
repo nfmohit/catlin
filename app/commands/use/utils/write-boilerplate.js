3f7cc53ad2aef5643cb73091087f270b70d539d8
nfmohit-wpmudev <nfmohit49@gmail.com>
2020-05-03 09:28:57 +0600

    Fixed linting errors on writeBoilerplate function

--- app/commands/use/utils/write-boilerplate.js
+++ app/commands/use/utils/write-boilerplate.js
@@ -10,37 +10,58 @@
  */
 import JSZip from 'jszip';
 import fs from 'fs';
+import * as ejs from 'ejs';
 
-const writeBoilerplate = ( boilerplate, download ) => {
-	JSZip.loadAsync( download ).then(
-		( zip ) => {
-			zip.folder( boilerplate ).forEach( ( relativePath, file ) => {
-				file.async( 'nodebuffer' ).then( ( fileContent ) => {
-					if ( ! file.dir ) {
-						console.log( `${ relativePath } is a file` );
-						fs.writeFileSync(
-							`${ process.cwd() }/tests/${ relativePath }`,
-							fileContent,
-							'utf8'
-						);
-					} else if ( file.dir ) {
-						console.log( `${ relativePath } is a directory` );
-						fs.mkdirSync(
-							`${ process.cwd() }/tests/${ relativePath }`
-						);
-						fs.writeFileSync(
-							`${ process.cwd() }/tests/${ relativePath }`,
-							fileContent,
-							'utf8'
-						);
-					}
-				} );
-			} );
-		},
-		function( error ) {
-			return error.message;
-		}
-	);
+/*
+ * Writes the boilerplate into the file system
+ *
+ * @param {string} boilerplate - Slug of the boilerplate.
+ * @param {buffer} download - The downloaded buffer.
+ * @param {object} answers - The answers to the questions.
+ * @param {object} questions - The questions.
+ */
+const writeBoilerplate = async (
+	boilerplate,
+	download,
+	answers,
+	questions
+) => {
+	try {
+		const { projectSlug } = answers;
+		const currentDir = process.cwd();
+		fs.mkdirSync( `${ currentDir }/${ projectSlug }` );
+
+		const replace = {};
+		questions.forEach( ( question ) => {
+			const thisQuestion = question.name;
+			const answer = answers[ thisQuestion ];
+			replace[ thisQuestion ] = answer;
+		} );
+
+		const zip = await JSZip.loadAsync( download );
+		zip.folder( boilerplate ).forEach( async ( relativePath, file ) => {
+			const isDir = file.dir;
+
+			if ( ! isDir ) {
+				const fileContent = await file.async( 'nodebuffer' );
+				const readFileContent = fileContent.toString( 'utf-8' );
+				const updatedFileContent = ejs.render(
+					readFileContent,
+					replace
+				);
+				const writePath = `${ currentDir }/${ projectSlug }/${ relativePath }`;
+				fs.writeFileSync( writePath, updatedFileContent, 'utf8' );
+			} else {
+				fs.mkdirSync(
+					`${ currentDir }/${ projectSlug }/${ relativePath }`
+				);
+			}
+		} );
+
+		return true;
+	} catch ( error ) {
+		return error;
+	}
 };
 
 export { writeBoilerplate };
